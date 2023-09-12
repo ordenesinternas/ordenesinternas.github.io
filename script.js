28663331dccb64c6ea6e19a88411776628362e26
ordenesinternas <141374096+ordenesinternas@users.noreply.github.com>
2023-09-12 13:45:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-function searchTable() {
+const searchInput = document.getElementById('search');
+const rows = document.querySelectorAll('tbody tr');
 
-  // Obtener el input y el filtro  
-  var input = document.getElementById("searchInput");
-  var filter = input.value.toLowerCase();
-  
-  // Obtener todas las filas de la tabla
-  var rows = document.querySelectorAll("table tbody tr");
+searchInput.addEventListener('keyup', function() {
 
-  // Recorrer las filas y ocultar las que no coinciden con el filtro
-  rows.forEach(function(row) {
+  const searchTerm = this.value.toLowerCase();
 
-    // Obtener el texto de la fila 
-    var text = row.textContent.toLowerCase();
-    
-    // Verificar si coincide con el filtro
-    if (text.indexOf(filter) === -1) {
-      // No coincide, ocultar fila
-      row.style.display = "none"; 
-    } else {
-      // Coincide, mostrar fila
-      row.style.display = "";
-    }
-
+  rows.forEach(row => {
+    const td = row.querySelector('td');
+    const text = td.textContent.toLowerCase();
+    row.style.display = text.indexOf(searchTerm) !== -1 ? '' : 'none';
   });
 
-}
+});
